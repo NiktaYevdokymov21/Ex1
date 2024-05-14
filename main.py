3daf297321c45a9d397343e16f9d7c300148b44d
NikitaYevdokymov <mrgamingpro170@gmail.com>
2024-05-14 21:06:37 +0300

    Loading of the game

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import random
 
 start = input('You`ce started the game "rock, paper". For the start press + or - for the end: ')
+
+if start == '+':
+    print('Loading...')
+    print('Loading is finished! Let`s start the game!!')
