b9a42ca23e471a6addfc2fb359cd477b35ac883c
NikitaYevdokymov <mrgamingpro170@gmail.com>
2024-05-14 21:10:45 +0300

    initial user_ball and rand_ball

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import random
 
-start = input('You`ce started the game "rock, paper". For the start press + or - for the end: ')
+start = input('You`ce started the game "rock, paper". For the start press "+" or "-" for the end: ')
 
 if start == '+':
     print('Loading...')
     print('Loading is finished! Let`s start the game!!')
+    print('3...2...1...')
+    print('If you want to exit press "-" ')
+    print('Wanna know score? Press "?" ')
+    user_ball = 0
+    rand_ball = 0
